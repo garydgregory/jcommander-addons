c6e2922a5dc3ae07682a9896031c0f3ec7a314ad
Gary Gregory <ggregory@apache.org>
2016-10-22 23:21:43 -0700

    Add Instant converter.

---
+++ src/main/java/com/garygregory/jcommander/converters/time/InstantConverter.java
@@ -0,0 +1,58 @@
+/**
+ *  Copyright (C) 2016 Gary Gregory. All rights reserved.
+ *
+ *  See the NOTICE.txt file distributed with this work for additional
+ *  information regarding copyright ownership.
+ *  
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.garygregory.jcommander.converters.time;
+
+import java.time.Instant;
+import java.time.Period;
+import java.time.format.DateTimeParseException;
+
+import com.garygregory.jcommander.converters.AbstractBaseConverter;
+
+/**
+ * Converts a {@link String} into a {@link Instant}.
+ * <p>
+ * For a description of the format, see {@link Instant#parse(CharSequence)}.
+ * </p>
+ * 
+ * @see Period
+ * @see Period#parse(CharSequence)
+ * 
+ * @since 1.0.0
+ * @author <a href="mailto:ggregory@garygregory.com">Gary Gregory</a>
+ */
+public class InstantConverter extends AbstractBaseConverter<Instant> {
+
+    /**
+     * Constructs a converter.
+     */
+    public InstantConverter() {
+        super(null);
+    }
+
+    @Override
+    public Instant convert(final String value) {
+        try {
+            return Instant.parse(value);
+        } catch (final DateTimeParseException e) {
+            throw newParameterException(value, Instant.class);
+        }
+    }
+
+}
---
+++ src/test/java/com/garygregory/jcommander/converters/time/InstantConverterTest.java
@@ -0,0 +1,45 @@
+/**
+ *  Copyright (C) 2016 Gary Gregory. All rights reserved.
+ *
+ *  See the NOTICE.txt file distributed with this work for additional
+ *  information regarding copyright ownership.
+ *  
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.garygregory.jcommander.converters.time;
+
+import java.time.Instant;
+
+import org.junit.Test;
+
+import com.garygregory.jcommander.converters.AbstractConverterTest;
+
+/**
+ * Tests {@link InstantConverter}.
+ * 
+ * @since 1.0.0
+ * @author <a href="mailto:ggregory@garygregory.com">Gary Gregory</a>
+ */
+public class InstantConverterTest extends AbstractConverterTest<Instant> {
+
+    public InstantConverterTest() {
+        super(new InstantConverter());
+    }
+
+    @Test
+    public void testInstant() {
+        testEquals(Instant.parse("2007-12-03T10:15:30.00Z"), "2007-12-03T10:15:30.00Z");
+    }
+
+}
