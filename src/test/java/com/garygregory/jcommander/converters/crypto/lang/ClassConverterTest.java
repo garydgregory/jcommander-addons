1527b95533a0ee8a10b99ce35ca84fe3839428d3
ggregory <ggregory@US-L-GG02.rocketsoftware.com>
2016-10-24 11:41:07 -0700

    Add class converter.

---
+++ src/test/java/com/garygregory/jcommander/converters/crypto/lang/ClassConverterTest.java
@@ -0,0 +1,45 @@
+/**
+ *  Copyright (C) 2016 Gary Gregory. All rights reserved.
+ *
+ *  See the NOTICE.txt file distributed with this work for additional
+ *  information regarding copyright ownership.
+ *  
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.garygregory.jcommander.converters.crypto.lang;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.garygregory.jcommander.converters.AbstractStringConverterBasicTest;
+import com.garygregory.jcommander.converters.lang.ClassConverter;
+
+/**
+ * Tests {@link ClassConverter}.
+ * 
+ * @since 1.0.0
+ * @author <a href="mailto:ggregory@garygregory.com">Gary Gregory</a>
+ */
+public class ClassConverterTest extends AbstractStringConverterBasicTest<Class> {
+
+    public ClassConverterTest() {
+        super(new ClassConverter());
+    }
+
+    @Test
+    public void testClass() {
+        Assert.assertEquals(ClassConverterTest.class, convert(ClassConverterTest.class.getName()));
+    }
+
+}
