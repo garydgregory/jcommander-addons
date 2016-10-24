978dbfd4fd08a8b16e31bc83d6da5a8d7574ddbe
ggregory <ggregory@US-L-GG02.rocketsoftware.com>
2016-10-24 11:29:45 -0700

    Add security Provider converter.

---
+++ src/main/java/com/garygregory/jcommander/converters/security/ProviderConverter.java
@@ -0,0 +1,54 @@
+/**
+ *  Copyright (C) 2016 Gary Gregory. All rights reserved.
+ *
+ *  See the NOTICE.txt file distributed with this work for additional
+ *  information regarding copyright ownership.
+ *  
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.garygregory.jcommander.converters.security;
+
+import java.security.Provider;
+import java.security.Security;
+
+import com.garygregory.jcommander.converters.AbstractBaseConverter;
+
+/**
+ * Converts a {@link String} into a SQL {@link Provider}.
+ * <p>
+ * For a description of the format, see {@link Security#getProvider(String)}.
+ * </p>
+ * 
+ * @see Provider
+ * @see Security
+ * @see Security#getProvider(String)
+ * 
+ * @since 1.0.0
+ * @author <a href="mailto:ggregory@garygregory.com">Gary Gregory</a>
+ */
+public class ProviderConverter extends AbstractBaseConverter<Provider> {
+
+    /**
+     * Constructs a converter.
+     */
+    public ProviderConverter() {
+        super(null, Provider.class, true);
+    }
+
+    @Override
+    protected Provider convertImpl(final String value) {
+        return Security.getProvider(value);
+    }
+
+}
---
+++ src/test/java/com/garygregory/jcommander/converters/security/ProviderConverterTest.java
@@ -0,0 +1,51 @@
+/**
+ *  Copyright (C) 2016 Gary Gregory. All rights reserved.
+ *
+ *  See the NOTICE.txt file distributed with this work for additional
+ *  information regarding copyright ownership.
+ *  
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.garygregory.jcommander.converters.security;
+
+import java.security.Provider;
+import java.security.Security;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.garygregory.jcommander.converters.AbstractStringConverterBasicTest;
+
+/**
+ * Tests {@link ProviderConverter}.
+ * 
+ * @since 1.0.0
+ * @author <a href="mailto:ggregory@garygregory.com">Gary Gregory</a>
+ */
+public class ProviderConverterTest extends AbstractStringConverterBasicTest<Provider> {
+
+    public ProviderConverterTest() {
+        super(new ProviderConverter());
+    }
+
+    @Test
+    public void testProvider() {
+        Provider expected = Security.getProviders()[0];
+        Provider actual = convert(expected.getName());
+        Assert.assertEquals(expected.getName(), actual.getName());
+        Assert.assertEquals(expected.getInfo(), actual.getInfo());
+        Assert.assertEquals(expected.getVersion(), actual.getVersion(), 0);
+    }
+
+}
